36975dbe81a2e4841d0af3a1ebed9de250f16620
JonnyDdesign <jondesserres@gmail.com>
2024-05-01 22:36:10 -0700

    Add an if statement to check if player entered q to quit the game and another if statement to check if player entered rock, paprer, or scissors. If player did not enter a valid option, the continue key word will stop the program until a valid input is entered.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -4,4 +4,9 @@ player_wins = 0
 computer_wins = 0
 
 while True:
-    player_input = input("Type Rock/Paper/Scissors or Q to quit.")
+    player_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if player_input == "q":
+        break
+
+    if player_input not in ["rock", "paper", "scissors"]:
+        continue
