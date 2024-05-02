c7cf7b773671e7a452588e2bf738a9d9604cfb89
JonnyDdesign <jondesserres@gmail.com>
2024-05-01 22:42:01 -0700

    Create a random number to represent rock, paper, or scissors being typed in. Created a variable called options that stores the list of options to choose and refactored the if statement to use the options variable.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,10 +3,15 @@ import random
 player_wins = 0
 computer_wins = 0
 
+options = ["rock", "paper", "scissors"]
+
 while True:
     player_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if player_input == "q":
         break
 
-    if player_input not in ["rock", "paper", "scissors"]:
+    if player_input not in options:
         continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
