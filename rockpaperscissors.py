faeed566cb55d2e77a30b074f085313deed1b24d
JonnyDdesign <jondesserres@gmail.com>
2024-05-02 19:36:39 -0700

    Added a computer choice to equate to a random options choice, followed by a print statement showing what the computer chose. Also added an if statement showing if the player chose rock and computer chose scissors, a print statement saying the player won, followed by their increasing by 1.

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -15,3 +15,11 @@ while True:
 
     random_number = random.randint(0, 2)
     # rock: 0, paper: 1, scissors: 2
+    computer_choice = options[random_number]
+    print("Computer chose: ", computer_choice + ".")
+
+    if player_input == "rock" and computer_choice == "scissors":
+        print("You won!")
+        player_wins += 1
+
+print("See ya next time!")
